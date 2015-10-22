f0e5abeaaeebe054e6507e88f10258c55a9b8fb5
Daniel Egan <djegan@casablanca.fritz.box>
2015-10-22 12:25:46 +1030

    Checkpoint

--- gitlib.sh
+++ gitlib.sh
@@ -1,9 +1,5 @@
 
-echo flag1.1
-
 function modificationsExist {
-
-    echo modificationsExist flag1
 
     if [ "$1" = "" ]
     then
@@ -21,10 +17,25 @@ function modificationsExist {
 
 }
 
+function unpushedCommitsExist {
+
+    if [ "$1" = "" ]
+    then
+        ROOT_DIR="."
+    else
+        ROOT_DIR="$1"
+    fi
+
+    if [ `git --git-dir="$ROOT_DIR/.git" --work-tree="$ROOT_DIR" status|grep '# Your branch is ahead'|wc -l` -gt 0 ]
+    then
+        return 0
+    else
+        return 1
+    fi
+
+}
+
 function verifyNoUncommittedChanges {
-
-    echo verifyNoUncommittedChanges flag1
-
 
     # Check there are no uncommitted changes
     if modificationsExist .
@@ -37,4 +48,15 @@ function verifyNoUncommittedChanges {
 
 }
 
-echo flag1.2
+function verifyNoUnpushedCommits {
+
+    # Check there are no "un-pushed" commits
+    if unpushedCommitsExist .
+    then
+        echo
+        echo ERROR: There are \"un-pushed\" commits
+        echo
+        exit 1
+    fi
+
+}
